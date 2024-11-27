94de720b8bab19f586049bf0d45ea6452cfe3f7e
sebastian camacho <secamc_93@hotmail.com>
2024-11-26 22:50:33 -0500

    edit link styles

--- starry-spectrum/astro.config.mjs
+++ starry-spectrum/astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://secamc93.github.io/PortafolioYo/',
   base: '/PortafolioYo/',
+  outDir: './dist', // Asegúrate de mantener esta configuración para la carpeta de salida
 });
 
